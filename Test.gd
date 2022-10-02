2a37929f32ea443df6396942039ed87571210ac5
ghsoares <ghsoares99795@gmail.com>
2022-10-02 14:46:46 -0300

    .

--- Test.gd
+++ Test.gd
@@ -2,4 +2,4 @@ tool
 extends Spatial
 
 func _process(delta: float) -> void:
-	Gizmo.add_line(Vector3.ZERO, Vector3.RIGHT * 32.0, Color.red, 64.0)
+	Gizmo.add_line(Vector3.ZERO, Vector3.RIGHT * 32.0, Color.red, 16.0)
