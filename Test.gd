37d10b5dd66d99a481eb9de04670d2f2c6bdf6be
ghsoares <ghsoares99795@gmail.com>
2022-10-04 13:56:35 -0300

    Final version fr

--- Test.gd
+++ Test.gd
@@ -2,5 +2,5 @@ tool
 extends Spatial
 
 func _process(delta: float) -> void:
-	Gizmo.add_line(Vector3.ZERO, Vector3.RIGHT * 128.0, Color.red, 60.0)
-	Gizmo.add_point(Vector3.ZERO, Color.green, 64.0)
+	Gizmo.add_line(Vector3.RIGHT * -128.0, Vector3.RIGHT * 128.0, Color.red, 32.0)
+	#Gizmo.add_point(Vector3.ZERO, Color.green, 64.0)
