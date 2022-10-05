0bb24e1e1eff265b21420a97c5c5b3f56aedfb02
ghsoares <ghsoares99795@gmail.com>
2022-10-05 14:25:50 -0300

    Yet another line shader version, clipping working

--- Test.gd
+++ Test.gd
@@ -1,11 +1,21 @@
 tool
 extends Spatial
 
+var t: float = 0.0
+
+func _ready() -> void:
+	Gizmo.connect("draw_gizmo", self, "on_draw_gizmo")
+
 func _process(delta: float) -> void:
-	Gizmo.add_line(
-		Vector3(-128.0, 0.0, 32.0), 
-		Vector3( 128.0, 0.0, 32.0),
-		Color.red, 
-		8.0
-	)
-	#Gizmo.add_point(Vector3.ZERO, Color.green, 64.0)
+	t += delta
+
+func on_draw_gizmo() -> void:
+	for i in 16:
+		var a: float = (i / 16.0) * TAU
+		a += t * TAU * ((1 + i) / 16.0)
+		var c: float = cos(a) * 16.0
+		var s: float = sin(a) * 16.0
+
+		var p: Vector3 = Vector3(c, 0.0, s) 
+
+		Gizmo.add_point(p, Color.red,  8.0)
--- addons/DebugDraw/Gizmo.gd
+++ addons/DebugDraw/Gizmo.gd
@@ -16,6 +16,9 @@ var point_geo: ImmediateGeometry
 # The mesh geometry
 var mesh_geo: ImmediateGeometry
 
+# Signal called to draw gizmo
+signal draw_gizmo()
+
 # Called when entering the tree
 func _enter_tree() -> void:
 	# Create the geometries
@@ -77,6 +80,9 @@ func _exit_tree() -> void:
 func _on_before_render() -> void:
 	# Is drawing
 	if state == 1:
+		# Emit signal
+		emit_signal("draw_gizmo")
+
 		# Finish drawing
 		line_geo.end()
 		point_geo.end()
@@ -111,66 +117,29 @@ func _on_after_render() -> void:
 
 		state = 1
 
-# Add a line
-func add_line(point_a: Vector3, point_b: Vector3, color: Color, radius: float = 1.0) -> void:
-	if state != 1: return
-
-	line_geo.set_color(color)
-
-	var off: Vector3 = point_b - point_a
-
-	var uv00: Vector2 = Vector2(-1.0, -1.0)
-	var uv01: Vector2 = Vector2( 1.0, -1.0)
-	var uv02: Vector2 = Vector2(-1.0,  1.0)
-	var uv03: Vector2 = Vector2( 1.0,  1.0)
-
-	line_geo.set_uv2(Vector2(radius, 0.0))
-	line_geo.set_normal(off)
-
-	line_geo.set_uv(uv00)
-	line_geo.add_vertex(point_a)
-
-	line_geo.set_uv(uv03)
-	line_geo.add_vertex(point_a)
-
-	line_geo.set_uv(uv02)
-	line_geo.add_vertex(point_a)
-
-	line_geo.set_uv(uv03)
-	line_geo.add_vertex(point_a)
-
-	line_geo.set_uv(uv00)
-	line_geo.add_vertex(point_a)
-
-	line_geo.set_uv(uv01)
-	line_geo.add_vertex(point_a)
-
-	var line_aabb: AABB = AABB(
-		point_a - Vector3.ONE * radius, off + Vector3.ONE * radius
-	)
-	if aabb.size.x == -1:
-		aabb = line_aabb
-	else:
-		aabb = aabb.merge(line_aabb)
-
 # Add a point
 func add_point(point: Vector3, color: Color, radius: float = 1.0) -> void:
 	if state != 1: return
 
 	point_geo.set_color(color)
+	point_geo.set_uv2(Vector2(radius, 0.0))
 
-	point_geo.set_uv(Vector2(-1.0, -1.0) * radius)
-	point_geo.add_vertex(point)
-	point_geo.set_uv(Vector2(-1.0,  1.0) * radius)
-	point_geo.add_vertex(point)
-	point_geo.set_uv(Vector2( 1.0, -1.0) * radius)
+	point_geo.set_uv(Vector2(-1.0, -1.0))
 	point_geo.add_vertex(point)
 
-	point_geo.set_uv(Vector2( 1.0, -1.0) * radius)
+	point_geo.set_uv(Vector2( 1.0,  1.0))
 	point_geo.add_vertex(point)
-	point_geo.set_uv(Vector2(-1.0,  1.0) * radius)
+
+	point_geo.set_uv(Vector2(-1.0,  1.0))
 	point_geo.add_vertex(point)
-	point_geo.set_uv(Vector2( 1.0,  1.0) * radius)
+
+	point_geo.set_uv(Vector2( 1.0,  1.0))
+	point_geo.add_vertex(point)
+
+	point_geo.set_uv(Vector2(-1.0, -1.0))
+	point_geo.add_vertex(point)
+
+	point_geo.set_uv(Vector2( 1.0, -1.0))
 	point_geo.add_vertex(point)
 
 	var point_aabb: AABB = AABB(
@@ -181,93 +150,160 @@ func add_point(point: Vector3, color: Color, radius: float = 1.0) -> void:
 	else:
 		aabb = aabb.merge(point_aabb)
 
-# Add a triangle
-func add_triangle(
-	vertex1: Vector3, vertex2: Vector3, vertex3: Vector3, 
-	color1: Color, color2: Color, color3: Color
+# Add a line
+func add_line(
+	point_a: Vector3, point_b: Vector3,
+	color_a: Color, color_b: Color,
+	radius_a: float = 1.0,
+	radius_b: float = 1.0
 ) -> void:
 	if state != 1: return
 
-	mesh_geo.set_color(color1)
-	mesh_geo.add_vertex(vertex1)
-	mesh_geo.set_color(color2)
-	mesh_geo.add_vertex(vertex2)
-	mesh_geo.set_color(color3)
-	mesh_geo.add_vertex(vertex3)
+	var off: Vector3 = point_b - point_a
 
-	var triangle_aabb: AABB = AABB(
-		vertex1, Vector3.ZERO
+	line_geo.set_uv2(Vector2(radius_a, radius_b))
+	line_geo.set_normal(off)
+	
+	line_geo.set_uv(Vector2(-1.0, -1.0))
+	line_geo.set_color(color_a)
+	line_geo.add_vertex(point_a)
+
+	line_geo.set_uv(Vector2( 1.0,  1.0))
+	line_geo.set_color(color_b)
+	line_geo.add_vertex(point_a)
+
+	line_geo.set_uv(Vector2(-1.0,  1.0))
+	line_geo.set_color(color_a)
+	line_geo.add_vertex(point_a)
+
+	line_geo.set_uv(Vector2( 1.0,  1.0))
+	line_geo.set_color(color_b)
+	line_geo.add_vertex(point_a)
+
+	line_geo.set_uv(Vector2(-1.0, -1.0))
+	line_geo.set_color(color_a)
+	line_geo.add_vertex(point_a)
+
+	line_geo.set_uv(Vector2( 1.0, -1.0))
+	line_geo.set_color(color_b)
+	line_geo.add_vertex(point_a)
+
+	var a: Vector3 = point_a - Vector3.ONE * radius_a
+	var b: Vector3 = point_b + Vector3.ONE * radius_b
+
+	var line_aabb: AABB = AABB(
+		a, b - a
 	)
-	triangle_aabb = triangle_aabb.expand(vertex2)
-	triangle_aabb = triangle_aabb.expand(vertex3)
 	if aabb.size.x == -1:
-		aabb = triangle_aabb
+		aabb = line_aabb
 	else:
-		aabb = aabb.merge(triangle_aabb)
+		aabb = aabb.merge(line_aabb)
 
-# Add a box
-func add_box(
-	center: Vector3, size: Vector3, color: Color
+# Add a polyline
+func add_polyline(
+	points: PoolVector3Array, 
+	colors: PoolColorArray, 
+	radius: PoolRealArray = PoolRealArray([1.0])
 ) -> void:
 	if state != 1: return
 
-	mesh_geo.set_color(color)
+	# Get number of points
+	var point_count: int = points.size()
 
-	var x0: float = center.x - size.x * 0.5
-	var y0: float = center.y - size.y * 0.5
-	var z0: float = center.z - size.z * 0.5
+	# Get number of colors
+	var color_count: int = colors.size()
 
-	var x1: float = center.x + size.x * 0.5
-	var y1: float = center.y + size.y * 0.5
-	var z1: float = center.z + size.z * 0.5
+	# Get number of radius
+	var radius_count: int = radius.size()
+	
+	# Assert that have at least two points
+	assert(point_count > 1)
 
-	# -X face
-	mesh_geo.add_vertex(Vector3(x0, y0, z0))
-	mesh_geo.add_vertex(Vector3(x0, y1, z0))
-	mesh_geo.add_vertex(Vector3(x0, y0, z1))
-	mesh_geo.add_vertex(Vector3(x0, y0, z1))
-	mesh_geo.add_vertex(Vector3(x0, y1, z0))
-	mesh_geo.add_vertex(Vector3(x0, y1, z1))
+	# For each line
+	for i in point_count - 1:
+		# Get the two points
+		var p0: Vector3 = points[i + 0]
+		var p1: Vector3 = points[i + 1]
 
-	# +X face
-	mesh_geo.add_vertex(Vector3(x1, y0, z1))
-	mesh_geo.add_vertex(Vector3(x1, y1, z1))
-	mesh_geo.add_vertex(Vector3(x1, y0, z0))
-	mesh_geo.add_vertex(Vector3(x1, y0, z0))
-	mesh_geo.add_vertex(Vector3(x1, y1, z1))
-	mesh_geo.add_vertex(Vector3(x1, y1, z0))
+		# Get the two colors
+		var c0: Color = colors[i + 0] if i + 0 < color_count else colors[color_count - 1]
+		var c1: Color = colors[i + 1] if i + 1 < color_count else colors[color_count - 1]
 
-	# -Y face
-	mesh_geo.add_vertex(Vector3(x0, y0, z0))
-	mesh_geo.add_vertex(Vector3(x0, y0, z1))
-	mesh_geo.add_vertex(Vector3(x1, y0, z0))
-	mesh_geo.add_vertex(Vector3(x1, y0, z0))
-	mesh_geo.add_vertex(Vector3(x0, y0, z1))
-	mesh_geo.add_vertex(Vector3(x1, y0, z1))
+		# Get the two radius
+		var r0: float = radius[i + 0] if i + 0 < radius_count else radius[radius_count - 1]
+		var r1: float = radius[i + 1] if i + 1 < radius_count else radius[radius_count - 1]
 
-	# +Y face
-	mesh_geo.add_vertex(Vector3(x0, y1, z1))
-	mesh_geo.add_vertex(Vector3(x0, y1, z0))
-	mesh_geo.add_vertex(Vector3(x1, y1, z1))
-	mesh_geo.add_vertex(Vector3(x1, y1, z1))
-	mesh_geo.add_vertex(Vector3(x0, y1, z0))
-	mesh_geo.add_vertex(Vector3(x1, y1, z0))
+		# Add the line
+		add_line(p0, p1, c0, c1, r0, r1)
 
-	# -Z face
-	mesh_geo.add_vertex(Vector3(x1, y0, z0))
-	mesh_geo.add_vertex(Vector3(x1, y1, z0))
-	mesh_geo.add_vertex(Vector3(x0, y0, z0))
-	mesh_geo.add_vertex(Vector3(x0, y0, z0))
-	mesh_geo.add_vertex(Vector3(x1, y1, z0))
-	mesh_geo.add_vertex(Vector3(x0, y1, z0))
+# Add a mesh
+func add_mesh(
+	vertices: PoolVector3Array,
+	colors: PoolColorArray,
+	indices: PoolIntArray
+) -> void:
+	if state != 1: return
 
-	# +Z face
-	mesh_geo.add_vertex(Vector3(x0, y0, z1))
-	mesh_geo.add_vertex(Vector3(x0, y1, z1))
-	mesh_geo.add_vertex(Vector3(x1, y0, z1))
-	mesh_geo.add_vertex(Vector3(x1, y0, z1))
-	mesh_geo.add_vertex(Vector3(x0, y1, z1))
-	mesh_geo.add_vertex(Vector3(x1, y1, z1))
+	# Get number of vertices
+	var vert_count: int = vertices.size()
+
+	# Get number of colors
+	var color_count: int = colors.size()
+
+	# Get number of indices
+	var id_count: int = indices.size()
+
+	# Must have multiple of 3 indices
+	assert(id_count % 3 == 0)
+	
+	# Mesh aabb
+	var mesh_aabb: AABB
+
+	# For each indice
+	for i in id_count:
+		# Get indice
+		var id: int = indices[i]
+
+		# Assert that indice is valid
+		assert(id >= 0 and id < vert_count)
+
+		# Get vertice
+		var vert: Vector3 = vertices[id]
+
+		# Get color
+		var color: Color = colors[id] if id < color_count else colors[color_count - 1]
+
+		# Add vertex
+		mesh_geo.set_color(color)
+		mesh_geo.add_vertex(vert)
+
+		# Add the vertice to aabb
+		if i == 0:
+			mesh_aabb = AABB(vert, Vector3.ZERO)
+		else:
+			mesh_aabb = mesh_aabb.expand(vert)
+
+	# Merge mesh aabb
+	if aabb.size.x == -1:
+		aabb = mesh_aabb
+	else:
+		aabb = aabb.merge(mesh_aabb)
+
+# # Add a box
+# func add_box(
+# 	center: Vector3, size: Vector3, color: Color
+# ) -> void:
+# 	if state != 1: return
+
+# 	# Get corners
+# 	var c000: Vector3 = Vector3(center.x - size.x, center.y - size.y, center.z - size.z)
+
+# 	# Add box mesh
+# 	add_mesh(
+# 		PoolVector3Array([
+# 			Vector3()
+# 		])
+# 	)
 
 
 
